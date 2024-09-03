a5e65182e24f31ae96af2a347ef0f4e4ec0388a9
Tao <shaoyu.tao@intel.com>
2024-09-03 22:30:36 +0800

    add proj

---
+++ pyflash/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const { spawn } = require('child_process');
+
+let mainWindow;
+let pythonProcess;
+
+app.on('ready', () => {
+    pythonProcess = spawn('python', ['app.py']);
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL('http://localhost:5000');
+
+    mainWindow.on('closed', () => {
+        pythonProcess.kill();
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
